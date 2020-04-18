01dd26380d7d4e05608169078c282c43da5bee2c
Kaleigh Spurio <kaleighastell@gmail.com>
2020-04-17 22:56:27 -0400

    added if statements for the different character options

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,10 @@
 var passwordButton = document.querySelector(".btn");
 var passwordOutput =  document.querySelector(".password");
-// var possibleCharacters = 
-var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", "~"];
-var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", "~"];
+var numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 
 // this bring up a prompt when the button is clicked, and asks the user what characters they want in the password
@@ -22,15 +22,45 @@ passwordButton.addEventListener("click", function(event) {
         console.log(lowerCase);
         console.log(upperCase);
     } else {
-        alert("That will not work. Please enter a number between 8 and 128. Click Generate Passwork again.");
+        alert("That will not work. Please click Generate Password again and enter a number between 8 and 128.");
     };
 
-    
-
-    if (specialCharacters == true && numbers == true && lowerCase == true && upperCase == true) {
-        var passwordArray = specials.concat(nums, lowers, uppers);
-        console.log(passwordArray);
+    if (specialCharacters == true) {
+        var passwordArray = specialsArray;
+    } else {
+        var passwordArray = [""]
     }
+
+    console.log(passwordArray);
+
+    if (numbers == true) {
+        var passwordArray = passwordArray.concat(numsArray);
+        console.log(passwordArray);
+    } else {
+        passwordArray = passwordArray;
+    }
+
+    if (lowerCase == true) {
+        var passwordArray = passwordArray.concat(lowersArray);
+        console.log(passwordArray);
+    } else {
+        passwordArray = passwordArray;
+    }
+
+    if (upperCase == true) {
+        passwordArray = passwordArray.concat(uppersArray);
+        console.log(passwordArray);
+    } else {
+        passwordArray = passwordArray;
+    }
+
+    console.log(passwordArray);
+
+
+    // if (specialCharacters == true && numbers == true && lowerCase == true && upperCase == true) {
+    //     var passwordArray = specialsArray.concat(numsArray, lowersArray, uppersArray);
+    //     console.log(passwordArray);
+    // }
     
     var randomPassword = " ";
     for (i = 0; i < characters; i++) { 
@@ -42,15 +72,3 @@ passwordButton.addEventListener("click", function(event) {
     
 });
 
-
-// function generatePassword(specialCharacters, numbers, lowerCase, upperCase) {
-//     if (specialCharacters = true && numbers = true && lowerCase = true && upperCase = true) {
-//         alert("wow!");
-//     }
-// } 
-// generatePassword(specialCharacters, numbers, lowerCase, upperCase);
-
-// if (specialCharacters == true && numbers == true && lowerCase == true && upperCase == true) {
-//     var passwordArray = specials.concat(numbers, lowerCase, upperCase);
-//     console.log(passwordArray);
-// }
