c528ce9f387fe57547c850d3a29696b16eb95f8e
Kaleigh Spurio <kaleighastell@gmail.com>
2020-07-27 15:00:28 -0400

    updated the README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # password-generator
-This password generator will prompt the user for a character length for their password, as well as for what types of characters the user wants included, and will generate a random password of the specified length.
 
-[Password Generator](https://kaleighspurio.github.io/password-generator/)
+## Description
+This password generator is a frontend application that utilizes Javascript, HTML5, CSS, and Bootstrap to randomly generate a password according to the options the user specifies using Math.random to select items from an array of characters that is created according the the users' specifications that were entered on the form.
+
+If the user does not specify a password length, or does not select at least one character option, they will be prompted to do so before the application will generate the password.
+
+## Installation
+Clone the repository and run with the live server.
+
+## Demo
+[Demo the Password Generator here!](https://kaleighspurio.github.io/password-generator/)
+
+## Screenshots
+
+12 Character password with special characters, numbers, upper and lowercase letters
+![12 character password](assets/images/Screen%20Shot%202020-07-27%20at%202.57.18%20PM.png)
+
+34 character password with special characters and lowercase letters
+![34 character password with special characters and lowercase letters](assets/images/Screen%20Shot%202020-07-27%20at%202.57.37%20PM.png)
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,8 +7,8 @@ const numbersInput = document.querySelector("#numbers");
 const lowercaseInput = document.querySelector("#lowercase");
 const uppercaseInput = document.querySelector("#uppercase");
 
-const specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", "~"];
-const numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", "~", "(", ")", "{", "}", "<", ">", "_", "-", "[", "]", "+", "="];
+const numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const lowersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const uppersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
@@ -16,19 +16,19 @@ const generatePassword = () => {
     // get the value from the character length input
     let characters = parseInt(charactersInput.value);
     console.log(characters);
+    // will not move forward if appropriate character length not provided.
     if (characters < 8 || characters > 128 || !characters) {
         alert("That will not work. Please enter a number between 8 and 128 for the password length.");
         return;
     }
 
-    // These if/else concatinate the appropriate arrays together into one large array to draw characters from.
+    // These if/else concatinate the appropriate arrays together into one large array to draw characters from depending on what was checked on the form.
     let passwordArray;
     if (specialsInput.checked) {
         passwordArray = specialsArray;
     } else {
         passwordArray = [];
     }
-
 
     if (numbersInput.checked) {
         passwordArray = passwordArray.concat(numsArray);
@@ -42,6 +42,7 @@ const generatePassword = () => {
         passwordArray = passwordArray.concat(uppersArray);
     } 
 
+    // will not move forward if at least one option is not checked
     if (!specialsInput.checked && !numbersInput.checked && !lowercaseInput.checked && !uppercaseInput.checked) {
         alert("You must confirm at least one type of character.  Please check off at least one option and try again.");
         return;
@@ -56,7 +57,6 @@ const generatePassword = () => {
     console.log(randomPassword);
     passwordOutput.textContent = randomPassword;
 }
-
 
 // this bring up a prompt when the button is clicked, and asks the user what characters they want in the password
 passwordButton.addEventListener("click", generatePassword);
